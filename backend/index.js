8da596088a35a0a26e258786274bef3f56726885
Dinesh Pawar <dsp70458@gmail.com>
2024-12-15 15:14:11 +0530

    Fix react-scripts issue

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,8 @@ const cors = require("cors");
 const newUserModel = require("./models/newUser");
 
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 mongoose.connect("mongodb://127.0.0.1:27017/newUser");
 
